6b30a3c34018a09fe84d0e1b70cdde76d5fe581e
Anurag Jabegu <ajabegu@myseneca.ca>
2025-05-23 22:21:53 -0400

    Completed OPS445 Lab 2

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Anurag Jabegu
+# Author ID: ajabegu
+# Date Created: 2025/05/24
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
+
